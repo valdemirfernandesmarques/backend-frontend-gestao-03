a2a7f80dc1f8ef974976038914d4ffbe9f00df7b
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-02-12 16:20:52 -0300

    Preparando deploy para AWS com banco trunas48 tudo pronto para produção-12-02-2026

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 
 const api = axios.create({
   // Usa variável de ambiente ou o próprio host para não quebrar em produção/rede local
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+ // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
  //baseURL: import.meta.env.VITE_API_URL || 'http://192.168.100.248:3000/api',
+ // timeout: 10000,
+  baseURL: import.meta.env.VITE_API_URL || 'http://13.58.7.29:3000/api',  // usando o servidor AWS
   timeout: 10000,
 });
 
