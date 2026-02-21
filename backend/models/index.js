70d01ac7273d36080a141a9f5f5d9b53aa1505b3
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-02-21 13:37:59 -0300

    Engineering Fix: Strict model loading order and associations

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,19 +1,27 @@
 const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 13908,
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 13908,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 const db = {};
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
-// Carregamento simples que seu sistema já usava
+// 1. CARREGAR TODOS OS MODELOS PRIMEIRO (ORDEM CRÍTICA)
 db.Escola = require("./Escola")(sequelize, DataTypes);
 db.Aluno = require("./Aluno")(sequelize, DataTypes);
 db.Professor = require("./Professor")(sequelize, DataTypes);
@@ -24,9 +32,14 @@ db.Matricula = require("./Matricula")(sequelize, DataTypes);
 db.Mensalidade = require("./Mensalidade")(sequelize, DataTypes);
 db.ProfessorModalidade = require("./ProfessorModalidade")(sequelize, DataTypes);
 
-// Ativa as associações
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) db[modelName].associate(db);
+// 2. EXECUTAR ASSOCIAÇÕES SOMENTE APÓS TODOS ESTAREM CARREGADOS
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName] && typeof db[modelName].associate === 'function') {
+    db[modelName].associate(db);
+  }
 });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
