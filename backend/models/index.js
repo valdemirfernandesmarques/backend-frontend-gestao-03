cc2b20b12d0fa4ec6908f6f743859efa43ad2888
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-02-21 13:06:42 -0300

    Fix: Forçando atualização completa do sistema e banco

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,48 +1,45 @@
 const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 13908,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 const db = {};
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// Importar Modelos
+// Importação dos Modelos
 db.Escola = require("./Escola")(sequelize, DataTypes);
 db.Aluno = require("./Aluno")(sequelize, DataTypes);
 db.Professor = require("./Professor")(sequelize, DataTypes);
+db.Funcionario = require("./Funcionario")(sequelize, DataTypes);
 db.Modalidade = require("./Modalidade")(sequelize, DataTypes);
 db.Turma = require("./Turma")(sequelize, DataTypes);
 db.Matricula = require("./Matricula")(sequelize, DataTypes);
 db.Mensalidade = require("./Mensalidade")(sequelize, DataTypes);
-db.Funcionario = require("./Funcionario")(sequelize, DataTypes);
 db.ProfessorModalidade = require("./ProfessorModalidade")(sequelize, DataTypes);
 
-// --- ASSOCIAÇÕES ---
-
-// Aluno / Turma / Matricula
-db.Matricula.belongsTo(db.Aluno, { foreignKey: "alunoId", as: "aluno" });
-db.Matricula.belongsTo(db.Turma, { foreignKey: "turmaId", as: "turma" });
-
-// Professor / Modalidade (O que resolve o erro 500 de professores)
-db.Professor.belongsToMany(db.Modalidade, { 
-  through: db.ProfessorModalidade, 
-  foreignKey: "professorId",
-  as: "modalidades" 
+// --- BLOCO DE ASSOCIAÇÕES ---
+// Isso garante que o Sequelize saiba que a coluna existe e como se liga
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
-db.Modalidade.belongsToMany(db.Professor, { 
-  through: db.ProfessorModalidade, 
-  foreignKey: "modalidadeId",
-  as: "professores" 
-});
-
-// Mensalidade
-db.Mensalidade.belongsTo(db.Matricula, { foreignKey: "matriculaId", as: "matricula" });
 
 module.exports = db;
