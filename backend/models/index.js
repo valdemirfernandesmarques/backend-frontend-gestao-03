6be10676e4d187f2c11f8e799d00539875b911aa
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-02-21 17:07:52 -0300

    Debug models carregados

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,46 +1,81 @@
+// backend/models/index.js
 const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
+// ===============================
+// CONEXÃO COM BANCO
+// ===============================
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME || "gestao_danca",
+  process.env.DB_USER || "root",
+  process.env.DB_PASS !== undefined ? process.env.DB_PASS : "",
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 13908,
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
     logging: false,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    timezone: "-03:00",
   }
 );
 
-const db = {};
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Conexão com MySQL OK!"))
+  .catch((err) => console.error("❌ Erro de conexão:", err));
 
+// ===============================
+// OBJETO DB
+// ===============================
+const db = {};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// ===============================
+// MODELS
+// ===============================
 db.Escola = require("./Escola")(sequelize, DataTypes);
+db.User = require("./User")(sequelize, DataTypes);
+db.PasswordResetToken = require("./PasswordResetToken")(sequelize, DataTypes);
+
 db.Aluno = require("./Aluno")(sequelize, DataTypes);
 db.Professor = require("./Professor")(sequelize, DataTypes);
 db.Funcionario = require("./Funcionario")(sequelize, DataTypes);
+
 db.Modalidade = require("./Modalidade")(sequelize, DataTypes);
 db.Turma = require("./Turma")(sequelize, DataTypes);
+
 db.Matricula = require("./Matricula")(sequelize, DataTypes);
 db.Mensalidade = require("./Mensalidade")(sequelize, DataTypes);
-db.ProfessorModalidade = require("./ProfessorModalidade")(sequelize, DataTypes);
-db.User = require("./User")(sequelize, DataTypes);
-db.TransacaoFinanceira = require("./TransacaoFinanceira")(sequelize, DataTypes);
+db.Pagamento = require("./Pagamento")(sequelize, DataTypes);
+
+db.LancamentoFinanceiro = require("./LancamentoFinanceiro")(sequelize, DataTypes);
+db.Comissao = require("./Comissao")(sequelize, DataTypes);
+
+db.Produto = require("./Produto")(sequelize, DataTypes);
 db.Venda = require("./Venda")(sequelize, DataTypes);
 db.VendaItem = require("./VendaItem")(sequelize, DataTypes);
 
+db.ProfessorModalidade = require("./ProfessorModalidade")(sequelize, DataTypes);
+
+db.IsencaoTaxa = require("./IsencaoTaxa")(sequelize, DataTypes);
+db.TransacaoFinanceira = require("./TransacaoFinanceira")(sequelize, DataTypes);
+
+// ===============================
+// DEBUG MODELS
+// ===============================
+console.log("🔎 DEBUG - MODELS CARREGADOS:");
+Object.keys(db).forEach((key) => {
+  if (key !== "sequelize" && key !== "Sequelize") {
+    console.log(key, "=>", typeof db[key]);
+  }
+});
+
+// ===============================
+// ASSOCIAÇÕES
+// ===============================
 Object.keys(db).forEach((modelName) => {
   if (db[modelName] && db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
