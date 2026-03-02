bc2a1125818dd61a1737a4c6c273d8bbeb2a8c62
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-03-02 18:51:56 -0300

    ajuste no arquivo escola

--- backend/models/escola.js
+++ backend/models/escola.js
@@ -1,31 +1,43 @@
-// backend/models/Escola.js
 module.exports = (sequelize, DataTypes) => {
-  const Escola = sequelize.define('Escola', {
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    isencaoAtiva: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    // ✅ NOVO: Coluna para armazenar a URL do logotipo
+  const Escola = sequelize.define('Escola', {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isencaoAtiva: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    // ✅ URL do logotipo da escola
     logoUrl: {
-        type: DataTypes.STRING,
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     }
-  });
+  }, {
+    // ✅ Força o nome exato da tabela para evitar erros no Linux (Render)
+    tableName: 'Escolas', 
+    timestamps: true
+  });
 
-  Escola.associate = (models) => {
-    Escola.hasMany(models.Matricula, {
-      foreignKey: 'escolaId',
-      as: 'matriculas',
-    });
-    Escola.hasMany(models.User, {
-      foreignKey: 'escolaId',
-      as: 'usuarios',
-    });
-  };
+  Escola.associate = (models) => {
+    // Uma escola tem muitas matrículas
+    Escola.hasMany(models.Matricula, {
+      foreignKey: 'escolaId',
+      as: 'matriculas',
+    });
+    // Uma escola tem muitos usuários (admins, secretários, etc)
+    Escola.hasMany(models.User, {
+      foreignKey: 'escolaId',
+      as: 'usuarios',
+    });
+    // ✅ Associação com comissões (Importante para o seu cadastro de comissão)
+    if (models.Comissao) {
+      Escola.hasMany(models.Comissao, {
+        foreignKey: 'escolaId',
+        as: 'comissoes',
+      });
+    }
+  };
 
-  return Escola;
+  return Escola;
 };
