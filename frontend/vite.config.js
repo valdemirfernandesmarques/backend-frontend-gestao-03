f401fc1950caad045f242a85eb7be4eb84ea93dc
Valdemir <valdemir.m@aluno.ifsc.edu.br>
2026-08-02 20:10:57 -0300

    Ajustando o sistema-02-08-2026

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://www.gestaoemdanca.com.br',
         changeOrigin: true,
       }
     }
